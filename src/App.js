cc27233e3c003466024bbb44ad001e8eeb6bb4a0
Dan Brown <brown.danny.j@gmail.com>
2020-09-12 16:11:50 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,21 @@ function App() {
     <div className="app">
 
       <div className="app__videos">
-        <Video />
-        <Video />
+        <Video url="https://v77.tiktokcdn.com/4f7ad59894d8b5d7855cda91f319a658/5f5d59cc/video/tos/useast2a/tos-useast2a-pve-0068/74b92f268af54eb3be3060fc41cd1932/?a=1233&br=3600&bt=1800&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009121729060101902091024A16609F&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M25vbHlscmpndzMzNDczM0ApO2k2OWY0NTtoNzNlNjw1M2dla2YxLy0vLWJfLS1jMTZzczIzLy0tYGA0L2M2MTRgLTQ6Yw%3D%3D&vl=&vr="
+        channel="savagemomlife"
+        description="bodacious dancing"
+        song="99 problems but react aint one"
+        likes={123}
+        messages={456}
+        shares={789}
+        />
+        <Video url="https://v77.tiktokcdn.com/4f7ad59894d8b5d7855cda91f319a658/5f5d59cc/video/tos/useast2a/tos-useast2a-pve-0068/74b92f268af54eb3be3060fc41cd1932/?a=1233&br=3600&bt=1800&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009121729060101902091024A16609F&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M25vbHlscmpndzMzNDczM0ApO2k2OWY0NTtoNzNlNjw1M2dla2YxLy0vLWJfLS1jMTZzczIzLy0tYGA0L2M2MTRgLTQ6Yw%3D%3D&vl=&vr="
+        channel="savagemomlife"
+        description="bodacious dancing"
+        song="99 problems but react aint one"
+        likes={123}
+        messages={456}
+        shares={789}/>
       </div>
 
       {/* app container */}
--- src/Video.js
+++ src/Video.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from 'react';
 import "./Video.css";
 import VideoFooter from './VideoFooter';
+import VideoSidebar from './VideoSidebar';
 
-function Video() {
+function Video({ url, channel, description, song, likes, messages, shares }) {
     const [playing, setPlaying] = useState(false);
     const videoRef = useRef(null);
 
@@ -23,15 +24,18 @@ function Video() {
                 className="video__player"
                 loop
                 ref={videoRef}
-                src="https://v77.tiktokcdn.com/4f7ad59894d8b5d7855cda91f319a658/5f5d59cc/video/tos/useast2a/tos-useast2a-pve-0068/74b92f268af54eb3be3060fc41cd1932/?a=1233&br=3600&bt=1800&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009121729060101902091024A16609F&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M25vbHlscmpndzMzNDczM0ApO2k2OWY0NTtoNzNlNjw1M2dla2YxLy0vLWJfLS1jMTZzczIzLy0tYGA0L2M2MTRgLTQ6Yw%3D%3D&vl=&vr="></video>
+                src={url}></video>
         
-        <VideoFooter />
+        <VideoFooter 
+            channel={channel}
+            description={description}
+            song={song}
+        />
+        <VideoSidebar likes={likes} shares={shares} messages={messages} />
 
-        {/* VideoFooter */}
-        {/* VideoSidebar */}
             
         </div>
-    )
+    );
 }
 
 export default Video
